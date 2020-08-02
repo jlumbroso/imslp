14b301b6262c07c195ec0469d47a159df055e24a
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-08-02 19:27:58 -0400

    IMSLP-specific `mwclient` code

---
+++ imslp/interfaces/mw_api.py
@@ -0,0 +1,46 @@
+
+import mwclient
+
+#
+IMSLP_HOST = "imslp.org"
+IMSLP_API_PATH = "/"
+IMSLP_SCHEME = "https"
+
+
+class ImslpMwClient(mwclient.Site):
+
+    _site = None
+
+    def __init__(self, username=None, password=None):
+        super().__init__(
+            host=IMSLP_HOST,
+            path=IMSLP_API_PATH,
+            scheme=IMSLP_SCHEME,
+        )
+
+        if username is not None and password is not None:
+            self.login(username=username, password=password)
+
+    def login(
+        self,
+        username: str = None,
+        password: str = None,
+        cookies: dict = None,
+        domain: str = None,
+    ):
+        # these cookies are necessary to allow for proper download of images/PDFs
+        combined_cookies = {
+            "imslp_wikiLanguageSelectorLanguage": "en",
+            "imslpdisclaimeraccepted": "yes",
+        }
+
+        # include user provided cookies
+        if cookies is not None:
+            combined_cookies.update(cookies)
+
+        super().login(
+            username=username,
+            password=password,
+            cookies=combined_cookies,
+            domain=domain,
+        )
