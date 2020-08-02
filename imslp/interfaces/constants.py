558b40cdae97050998dfbdd48a2c2c49f1750310
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-08-02 19:39:59 -0400

    File `constants.py` to contain some hard-coded cross-module constants

---
+++ imslp/interfaces/constants.py
@@ -0,0 +1,23 @@
+
+__author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
+
+__all__ = [
+    "IMSLP_SUBCATEGORIES"
+]
+
+
+# These are the subcategories available for some categories
+# NOTE: should this be an enum?
+IMSLP_SUBCATEGORIES = [
+    "Compositions",
+    "Collaborations",
+    "Collections",
+
+    "As Arranger",
+    "As Copyist",
+    "As Dedicatee",
+    "As Editor",
+
+    "Books",
+    "Pasticcios",
+]
