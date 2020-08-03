8f54e84a347f3fc915b82579d8f3ea59f7a3eaba
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2020-08-03 02:57:07 -0400

    Changed how score records are hashed internally (use only `permlink`)

--- imslp/interfaces/internal.py
+++ imslp/interfaces/internal.py
@@ -1,7 +1,13 @@
 
+import glob
+import json
+import os
 import typing
+import zipfile
 
 import requests
+
+import imslp
 
 
 __author__ = "Jérémie Lumbroso <lumbroso@cs.princeton.edu>"
@@ -38,27 +44,14 @@ _cache_works = None
 class HashablePageRecord(dict):
     """
     Subclass of a dictionary that can contain an IMSLP API record that
-    can be hashed, either using the `pageid` property, which every sheet
-    music record should have—or if not present, then a string hash of the
-    `permlink` of the record.
+    can be hashed, using a string hash of the `permlink` of the record.
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
     def __hash__(self):
-        try:
-            hash_str = self.get("intvals", dict()).get("pageid", None)
-            hash_id = int(hash_str)
-        except ValueError:
-            hash_id = None
-        except TypeError:
-            hash_id = None
-
-        if hash_id is None:
-            hash_id = hash(self["permlink"])
-            #raise ValueError("unhashable record: {}".format(self))
-
+        hash_id = hash(self["permlink"])
         return hash_id
 
 
@@ -128,14 +121,94 @@ def list_works(start: int = 0, count: int = None, cache: bool = True):
     )))
 
 
-def load_cache(start: int = 0, count: int = None, cache: bool = True):
+def reset_cache(from_file: bool = True) -> typing.NoReturn:
+    """
+
+    :param from_file:
+    :return:
+    """
+
     global _cache_people, _cache_works
+
+    _cache_people = None
+    _cache_works = None
+
+    return load_cache(from_file=from_file)
+
+
+# noinspection PyBroadException
+def load_cache(from_file: bool = True) -> typing.NoReturn:
+    """
+    Loads a copy of the IMSLP people and works to memory cache, so that
+    subsequent queries will be faster; if `from_file` is `True`, this
+    method will attempt to load a partial cache from the package's
+    internal files and only request newer entries from IMSLP. This
+    considerably speeds up operations by about 4 minutes on a typical
+    high-speed bandwidth.
+
+    :param from_file: Determines whether to use package's internal cache
+    :return:
+    """
+
+    global _cache_people, _cache_works
+
+    # this is slow when not using the disk cache (which makes the
+    # package quite large unfortunately! but at least not routinely
+    # downloading 60 MB)
+    #
     # on my computer + with my bandwidth (just as order of magnitude):
     # - list_people: 164880 items, 200 seconds to load
     # - list_works: 30559 items, 35 seconds to load
 
     if _cache_people is None:
-        _cache_people = list_people(cache=False)
+
+        if from_file:
+            # Try to load from file
+            possible_files = sorted(
+                list(map(os.path.abspath,
+                         map(os.path.expanduser,
+                             glob.glob(os.path.join(
+                                 imslp.__path__[0],
+                                 "../cache/imslp-people-cache-*.zip"))))),
+                reverse=True)
+
+            if len(possible_files) > 0:
+                zf = zipfile.ZipFile(possible_files[0], mode="r")
+                partial_contents = json.loads(zf.read(zf.namelist()[0]))
+                try:
+                    remainder_contents = list_people(start=len(partial_contents), cache=False)
+                    _cache_people = partial_contents + remainder_contents
+                except:
+                    _cache_people = None
+
+                _cache_people = list(map(HashablePageRecord, _cache_people))
+
+        if _cache_people is None:
+            _cache_people = list_people(cache=False)
 
     if _cache_works is None:
-        _cache_works = list_works(cache=False)
+
+        if from_file:
+            # Try to load from file
+            possible_files = sorted(
+                list(map(os.path.abspath,
+                         map(os.path.expanduser,
+                             glob.glob(os.path.join(
+                                 imslp.__path__[0],
+                                 "../cache/imslp-works-cache-*.zip"))))),
+                reverse=True)
+
+            if len(possible_files) > 0:
+                zf = zipfile.ZipFile(possible_files[0], mode="r")
+                partial_contents = json.loads(zf.read(zf.namelist()[0]))
+                try:
+                    remainder_contents = list_works(start=len(partial_contents), cache=False)
+                    _cache_works = partial_contents + remainder_contents
+                except:
+                    _cache_works = None
+
+                _cache_works = list(map(HashablePageRecord, _cache_works))
+
+        if _cache_works is None:
+            _cache_works = list_works(cache=False)
+
